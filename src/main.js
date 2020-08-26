cb3af42e16bb1001a9b5d522d6576953c2825000
shipeiqi1 <1178932475@qq.com>
2020-08-26 21:47:51 +0800

    初始化项目完毕

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+// 引入vue 引入根组件 引入路由
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入全局css
+import './styles/bess.less'
+import './styles/iconfont.css'
+// 引入全局的vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
+// 隐藏vue默认提示信息
 Vue.config.productionTip = false
 
 new Vue({
+  // 把路由挂载到组件上
   router,
+  // 渲染App.vue作为根组件
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是登录组件
+    <router-link to="register">
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
